edd3097da63538815533c83d875f6632a3a346d0
SineSam <ssnngcongo@gmail.com>
2024-03-07 03:46:00 -0800

    created script to gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_ID = sys.argv[1]
-    jsonplaceholder = 'https://jsonplaceholder.typicode.com/employee'
-    url = f'{jsonplaceholder}/employee/{employee_ID}'
+    jsonplaceholder = 'https://jsonplaceholder.typicode.com'
+    url = f'{jsonplaceholder}/users/{employee_ID}'
 
     # Make a GET request to API
     response = requests.get(url)
@@ -21,12 +21,12 @@ if __name__ == "__main__":
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
         employee_name = response.json().get('name')
-        todo_url = f'{url}/todo'
-        retrieve_todo = request.get(todo_url)
+        todo_url = f'{jsonplaceholder}/todo?userId={employee_ID}'
+        retrieve_todo = requests.get(todo_url)
         tasks = retrieve_todo.json()
 
         # Retrieve tasks
-        complete_tasks = [task for taks in tasks if task.get('complete')]
+        complete_tasks = [task for task in tasks if task.get('complete')]
         print("Employee {} is done with tasks({}/{})):".format(employee_name,
             len(complete_tasks), len(tasks)))
         for task in complete_tasks:
