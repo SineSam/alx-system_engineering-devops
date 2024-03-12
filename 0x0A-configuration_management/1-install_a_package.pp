8559ed42ce26c7deef37465a812fafd2132b2927
SineSam <ssnngcongo@gmail.com>
2024-03-11 17:58:20 -0700

    updated manifest to be puppet-lint documentation style

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install a package
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/bin',
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => '/usr/bin',
   environment => 'HOME=/root',
 }
